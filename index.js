4e0222081b2d3b858210e5d110718afc17a74a47
DSohm <97775027+DSohm@users.noreply.github.com>
2022-08-28 14:27:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que dirias que si Lu <3 uwu  https://www.youtube.com/watch?v=sfv0xWONROg')
+    alert('Sabia que dirias que si Lu <3 uwu')
 });
+
+yesBtn.addEventListener('mouseover',function (event) {
+   event.target.style. background = "#08fc31"; 
+});
+yesBtn.addEventListener('mouseout',function (event) {
+    event.target.style. background = "white"; 
+ });
+
+  
+  
+ 
 
 const noBtn = document.querySelector('#noBtn');
 
@@ -13,3 +25,4 @@ noBtn.addEventListener('mouseover', function () {
     noBtn.style.setProperty('left',randomX+'%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
 })
+
